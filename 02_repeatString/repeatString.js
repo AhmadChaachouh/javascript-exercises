6f27e506137d0e1996b8fc5e4420a9910b189873
Ahmad Chaachouh <ahmad.chaachouh.cad@gmail.com>
2023-01-18 11:24:29 +0200

    Solved ex02

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,13 @@
-const repeatString = function() {
-
+const repeatString = function(text, num) {
+    let t = "";
+    if(num < 0){
+        return "ERROR";
+    }else{
+       for(i = 0; i < num; i++){
+            t = t.concat(text);
+        }
+    }
+    return t;
 };
 
 // Do not edit below this line
