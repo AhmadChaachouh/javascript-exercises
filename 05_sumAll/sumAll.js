a9d21c33bcdb6534fdff5e4ea7c771a6ab37a410
Ahmad Chaachouh <ahmad.chaachouh.cad@gmail.com>
2023-01-18 22:54:32 +0200

    solved ex05

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,21 @@
-const sumAll = function() {
-
+const sumAll = function(a,b) {
+    if(typeof(a) != "number" || typeof(b) != "number"){
+        return "ERROR";
+    }
+    if(a < 0 || b < 0){
+        return "ERROR";
+    }
+    if(a > b){
+        let tmp = 0;
+        tmp = b;
+        b = a;
+        a = tmp;
+    }
+    let s = 0;
+    for (i = a; i <= b; i++){
+        s = s + i;
+    }
+    return s;
 };
 
 // Do not edit below this line
