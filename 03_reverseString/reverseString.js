bdc4eb0abe0dca9d9b4b2db3cb97e74d86f4205d
Ahmad Chaachouh <ahmad.chaachouh.cad@gmail.com>
2023-01-18 20:58:24 +0200

    solved ex03

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,9 @@
-const reverseString = function() {
-
+const reverseString = function(string) {
+    let r = "";
+    for(i = string.length - 1; i >= 0; i--){
+        r = r.concat(string[i]);
+    }
+    return r;
 };
 
 // Do not edit below this line
