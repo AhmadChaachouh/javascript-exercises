b469217704befd849016ba12de716e9302d66213
Ahmad Chaachouh <ahmad.chaachouh.cad@gmail.com>
2023-01-19 17:52:29 +0200

    solved ex10

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,14 @@
-const fibonacci = function() {
-
+const fibonacci = function(n) {
+    if(n < 0){return "OOPS";}
+    else{let previous = 0;
+    let current = 1;
+    let tmp = 0
+    for(i=1 ; i<parseInt(n) ; i++){
+        tmp = current
+        current = current + previous;
+        previous = tmp;
+    }
+    return current;}
 };
 
 // Do not edit below this line
