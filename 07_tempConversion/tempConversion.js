1ecdee8550de02369fa4fab4e42b13d18daae51b
Ahmad Chaachouh <ahmad.chaachouh.cad@gmail.com>
2023-01-19 14:06:10 +0200

    solved ex07

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,19 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(fah) {
+  let c = (fah -32)/1.8;
+  if(c == Math.floor(c)){
+    return c;
+  }else{
+  return parseFloat(c.toFixed(1));
+  }
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(c) {
+  let f = 1.8*c +32;
+  if(f == Math.floor(f)){
+    return f;
+  }else{
+  return parseFloat(f.toFixed(1));
+  }
 };
 
 // Do not edit below this line
