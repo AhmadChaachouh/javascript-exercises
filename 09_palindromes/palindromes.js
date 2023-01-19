cabaf273964ea678f731cd1f6a51a4f6e775225d
Ahmad Chaachouh <ahmad.chaachouh.cad@gmail.com>
2023-01-19 17:37:24 +0200

    solved ex09

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,14 @@
-const palindromes = function () {
-
+const palindromes = function (string) {
+    let r = string.replace(/\s/g,"");
+    r = r.replace(/,/g,"");
+    r = r.replace(".","");
+    r = r.replace("!","");
+    alert(r.split("").reverse().join("").toUpperCase());
+    if(r.toUpperCase() == r.split("").reverse().join("").toUpperCase()){
+        return true;
+    }else{
+        return false;
+    }
 };
 
 // Do not edit below this line
