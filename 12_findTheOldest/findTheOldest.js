d3ffe18333192d0a37a33b9a02f2118bf6e93e8e
Ahmad Chaachouh <ahmad.chaachouh.cad@gmail.com>
2023-01-20 13:01:44 +0200

    solved ex12

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,18 @@
-const findTheOldest = function() {
-
+const findTheOldest = function(people) {
+    let oldestindex = 0;
+    max_age = 0;
+    for(i=0; i<people.length; i++){
+        if(people[i].yearOfDeath == undefined){
+            if((2023 - people[i].yearOfBirth) > (people[oldestindex].yearOfDeath - people[oldestindex].yearOfBirth)){
+                oldestindex = i;
+            }
+        }else{
+            if((people[i].yearOfDeath - people[i].yearOfBirth) > (people[oldestindex].yearOfDeath - people[oldestindex].yearOfBirth)){
+                oldestindex = i;
+          }
+        }
+    }
+    return people[oldestindex];
 };
 
 // Do not edit below this line
